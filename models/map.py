f1383e92414cba6e6e207e47e96c2cb5a11bc06c
Molya Ayaz <ayazova7@gmail.com>
2020-10-27 23:29:14 +0600

    Initialized map

---
+++ models/map.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Map:
+    def __init__(self,
+                 screen,
+                 tasks,
+                 background,
+                 players,
+                 walls,
+                 spawn):  # spawn - это изначальное место, откуда начинается игра
+
+        self.screen = screen
+        self.background = pygame.image.load(background)
+        self.tasks = tasks
+        self.players = players
+        self.walls = walls
+        self.spawn = spawn
+
+    def display(self):
+        self.screen.blit(self.background)
