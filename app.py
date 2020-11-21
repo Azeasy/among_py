eea9dfa13358346ac74c16f43c0d7d056247172b
Azeasy <Azeasy19@gmail.com>
2020-11-21 22:40:53 +0600

    player move, animate

--- app.py
+++ app.py
@@ -3,34 +3,55 @@ from models.player import Player
 
 pygame.init()
 size = (1024, 768)
-fps = 20  # Frames per second
+fps = 30  # Frames per second
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 x = 100
 y = 100
 
-player = Player("test_block.png", screen, "Azeasy", x=x, y=y)
-player2 = Player("test_block.png", screen, "Azeasy", x=x + 30, y=y - 10)
+player = Player("static/images/green.png", screen, "Azeasy", x=x, y=y, speed=2)
 
-i = 0
-# Game loop
+animation_step = 0
 done = False
+direction = set()
+
+# Game loop
 while not done:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            direction.add('up')
+        if keys[pygame.K_DOWN]:
+            direction.add('down')
+        if keys[pygame.K_RIGHT]:
+            direction.add('right')
+        if keys[pygame.K_LEFT]:
+            direction.add('left')
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:  # What exactly did we press?
+                direction.remove('up')
+            if event.key == pygame.K_DOWN:
+                direction.remove('down')
+            if event.key == pygame.K_RIGHT:
+                direction.remove('right')
+            if event.key == pygame.K_LEFT:
+                direction.remove('left')
+
+    player.move(direction=direction)
 
     # Draw
     screen.fill((100, 100, 255))
 
-    player.position = (x + i, y + i)
-    player.display()
-
-    player2.position = (x + i + 30, y + i - 10)
-    player2.display()
-
-    i += 1
+    freq = fps // 3
+    animation_step += 1
+    animation_step %= freq
+    player.display(direction=direction, step=animation_step, frequency=freq//2)
 
     pygame.display.flip()
     clock.tick(fps)
--- models/player.py
+++ models/player.py
@@ -2,17 +2,81 @@ import pygame
 
 
 class Player:
-    image_size = 50
+    image_size = 100
 
-    def __init__(self, image_path, screen, nickname, tasks=[], x=0, y=0):
+    def __init__(self, image_path, screen, nickname, tasks=[], x=0, y=0, speed=1):
         image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(image,
                                             (self.image_size,
                                              self.image_size))
+
+        image_left = pygame.image.load(image_path.replace('.png', '_left.png'))
+        self.image_left = pygame.transform.scale(image_left,
+                                                 (self.image_size,
+                                                  self.image_size))
+        image_right = pygame.image.load(image_path.replace('.png', '_right.png'))
+        self.image_right = pygame.transform.scale(image_right,
+                                                  (self.image_size,
+                                                   self.image_size))
+        image_right_jump = pygame.image.load(image_path.replace('.png', '_right_jump.png'))
+        self.image_right_jump = pygame.transform.scale(image_right_jump,
+                                                       (self.image_size,
+                                                        self.image_size))
+        image_left_jump = pygame.image.load(image_path.replace('.png', '_left_jump.png'))
+        self.image_left_jump = pygame.transform.scale(image_left_jump,
+                                                      (self.image_size,
+                                                       self.image_size))
         self.screen = screen
         self.nickname = nickname
         self.tasks = tasks
         self.position = (x, y)
+        self.speed = speed
+        self.animate = False
+        self.cur_dir = 'right'
 
-    def display(self):
+    def display(self, direction, step=0, frequency=10):
+        animate = len(direction) > 0
+        if 'left' in direction:
+            self.image = self.image_left
+            self.cur_dir = 'left'
+        elif 'right' in direction:
+            self.image = self.image_right
+            self.cur_dir = 'right'
+
+        if animate:
+            if self.cur_dir == 'left':
+                if step < frequency:
+                    self.image = self.image_left_jump
+                else:
+                    self.image = self.image_left
+            elif self.cur_dir == 'right':
+                if step < frequency:
+                    self.image = self.image_right_jump
+                else:
+                    self.image = self.image_right
+        else:
+            if self.cur_dir == 'left':
+                self.image = self.image_left
+            elif self.cur_dir == 'right':
+                self.image = self.image_right
+
         self.screen.blit(self.image, self.position)
+
+    def move(self, direction):
+        cur_pos = self.position
+        cur_x, cur_y = cur_pos[0], cur_pos[1]
+
+        reduce_speed = 1
+        if len(direction) > 1:
+            reduce_speed = 2 ** 0.5
+
+        if 'up' in direction:
+            cur_y -= 5 * self.speed / reduce_speed
+        if 'down' in direction:
+            cur_y += 5 * self.speed / reduce_speed
+        if 'left' in direction:
+            cur_x -= 5 * self.speed / reduce_speed
+        if 'right' in direction:
+            cur_x += 5 * self.speed / reduce_speed
+
+        self.position = (cur_x, cur_y)
